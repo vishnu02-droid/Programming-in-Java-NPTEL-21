fdb71242efc1a33239f5d0b155db56c3faadced3
Vishnuraj Saravanan <76027763+vishnu02-droid@users.noreply.github.com>
2021-08-09 11:22:27 +0530

    Update Exercise-2_1.java

--- WEEK -2/Exercise-2_1.java
+++ WEEK -2/Exercise-2_1.java
@@ -1,2 +1,40 @@
+// This is the class named School
+class School { 
+    // This is a method in class School
+    public void print() { 
+		System.out.println("Hi! I class SCHOOL."); 
+    } 
+} 
+// This is the class named Student
+class Student { 
+	// This is a method in class Student
+    public void print() { 
+		System.out.println("Hi! I am class STUDENT"); 
+    } 
+}
 
-https://github.com/vishnu02-droid/Programming-in-Java-NPTEL-21/blob/3633eb3acc50eded6e03acd2bb0a9b8be89995d2/WEEK-2
+public class Question21{ 
+    public static void main(String args[]){
+// Create an object of class Student
+
+// Call 'print()' method of class Student 
+
+// Create an object of class School
+
+// Call 'print()' method of class School
+ 
+	// Create an object of inner class Student
+ // This is the outer class named School
+ 
+	// Create an object of inner class Student
+		
+	// Access the 'print()' method of the inner class Student using the inner class object
+	Student stu = new Student();
+ 
+	stu.print();
+
+	School sch = new School();
+
+	sch.print();
+     } 
+}
